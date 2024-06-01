54c107844322b1ef99ecdd80cf68e1a582378941
KauanPaschoal <kauan.psantos@sptech.school>
2024-06-01 16:13:02 -0300

    Alteração no css dos cards de texto da sessão sociedade do site

--- 2.API/public/js/cadastro.js
+++ 2.API/public/js/cadastro.js
@@ -1,68 +1,66 @@
 function cadastrar() {
-    aguardar();
+  aguardar();
 
-    var nomeVar = nome_input.value;
-    var emailVar = email_input.value;
-    var senhaVar = senha_input.value;
-    var confirmacaoSenhaVar = confirmacao_senha_input.value;
+  var nomeVar = nome_input.value;
+  var emailVar = email_input.value;
+  var senhaVar = senha_input.value;
+  var confirmacaoSenhaVar = confirmacao_senha_input.value;
 
-    if (
-      nomeVar == "" ||
-      emailVar == "" ||
-      senhaVar == "" ||
-      confirmacaoSenhaVar == ""
-    ) {
-      cardErro.style.display = "block";
-      mensagem_erro.innerHTML =
-        "(Mensagem de erro para todos os campos em branco)";
+  if (
+    nomeVar == "" ||
+    emailVar == "" ||
+    senhaVar == "" ||
+    confirmacaoSenhaVar == ""
+  ) {
+    cardErro.style.display = "block";
+    mensagem_erro.innerHTML =
+      "(Mensagem de erro para todos os campos em branco)";
 
-      finalizarAguardar();
-      return false;
-    }else if(emailVar.indexOf('@') < 0 && emailVar.indexOf('#')){
-      cardErro.style.display = "block";
-      mensagem_erro.innerHTML =
-        "(O seu Email deve conter pelo menos 1 caracter especial e um .)";
-        return false;
-    } else if(senhaVar != confirmacaoSenhaVar){
-      cardErro.style.display = "block";
-      mensagem_erro.innerHTML =
-        "(Sua confirmação de senha deve ser igual a sua senha)";
-        return false;
-    }else if(senhaVar.length < 7){
-      cardErro.style.display = "block";
-      mensagem_erro.innerHTML =
-        "(Sua senha deve conter ao menos 6 caracteres)";
-        return false;
-    } else {
-      setInterval(sumirMensagem, 5000);
-      cardErro.style.display = "block";
-      mensagem_erro.innerHTML =
-        "(Sendo redirecionado ao login)";
-        
-        fetch("/usuarios/cadastrar", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            nomeServer: nomeVar,
-            emailServer: emailVar,
-            senhaServer: senhaVar,
-          }),
-        })
-        .then(function (resposta) {
+    finalizarAguardar();
+    return false;
+  } else if (emailVar.indexOf("@") < 0 && emailVar.indexOf("#")) {
+    cardErro.style.display = "block";
+    mensagem_erro.innerHTML =
+      "(O seu Email deve conter pelo menos 1 caracter especial)";
+    return false;
+  } else if (senhaVar != confirmacaoSenhaVar) {
+    cardErro.style.display = "block";
+    mensagem_erro.innerHTML =
+      "(Sua confirmação de senha deve ser igual a sua senha)";
+    return false;
+  } else if (senhaVar.length >= 6) {
+    cardErro.style.display = "block";
+    mensagem_erro.innerHTML = "(Sua senha deve conter ao menos 6 caracteres)";
+    return false;
+  } else {
+    setInterval(sumirMensagem, 5000);
+    cardErro.style.display = "block";
+    mensagem_erro.innerHTML = "(Sendo redirecionado ao login)";
+
+    fetch("/usuarios/cadastrar", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        nomeServer: nomeVar,
+        emailServer: emailVar,
+        senhaServer: senhaVar,
+      }),
+    })
+      .then(function (resposta) {
         console.log("resposta: ", resposta);
-        
+
         if (resposta.ok) {
           cardErro.style.display = "block";
-          
+
           mensagem_erro.innerHTML =
-          "Cadastro realizado com sucesso! Redirecionando para tela de Login...";
-          
+            "Cadastro realizado com sucesso! Redirecionando para tela de Login...";
+
           setTimeout(() => {
             window.location = "login.html";
           }, "2000");
-          
+
           limparFormulario();
           finalizarAguardar();
         } else {
@@ -73,11 +71,11 @@ function cadastrar() {
         console.log(`#ERRO: ${resposta}`);
         finalizarAguardar();
       });
-      
-      return false;
-    }
-  }
 
-  function sumirMensagem() {
-    cardErro.style.display = "none";
+    return false;
   }
+}
+
+function sumirMensagem() {
+  cardErro.style.display = "none";
+}
